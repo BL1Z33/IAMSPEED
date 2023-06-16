d9b28a17ce3018d44c44b9ffc9cbae459607bbee
BL1Z33 <134890107+BL1Z33@users.noreply.github.com>
2023-06-16 08:41:35 -0400

    Add one file mode

--- IAMSPEED.bat
+++ IAMSPEED.bat
@@ -1,7 +1,21 @@
 @echo off
-mkdir Folder
-cd Folder
+mkdir IAMSPEED
+cd IAMSPEED
+set /p MODE="If you'd like one file mode, type 'one'. If you don't, type 'spam'."
+goto %MODE%
+
+:one
+mkdir onefile
+cd onefile
+set /p COPY="Type what you want to copy: "
+goto one-a
+:one-a
+echo %COPY% >> %RANDOM%.txt
+goto one-a
+
+:spam
 set /p COPY="Type what you want to spam copy: "
+goto a
 :a
 echo %COPY% > %RANDOM%.txt
 goto a
